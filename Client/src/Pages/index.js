bb965b4e4e5bbbe85cb42ac78d150e84b31f0d31
mrahulsit <mrahuls104@gmail.com>
2024-07-14 02:44:42 +0530

    nochange

--- Client/src/Pages/index.js
+++ Client/src/Pages/index.js
@@ -1,9 +1,5 @@
-import Fitness from './Fitness.jsx'
-import MustHave from './MustHave.jsx'
-import WomenBaby from './WomenBaby.jsx'
+import Fitness from "./Fitness.jsx";
+import MustHave from "./MustHave.jsx";
+import WomenBaby from "./WomenBaby.jsx";
 
-export {
-    Fitness,
-    MustHave,
-    WomenBaby
-}
+export { Fitness, MustHave, WomenBaby };
