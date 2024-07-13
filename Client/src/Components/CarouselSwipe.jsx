deafb608618c05b896510f8515cb6d1bdf28be0f
mrahulsit <mrahuls104@gmail.com>
2024-07-14 00:30:27 +0530

    changes

--- Client/src/Components/CarouselSwipe.jsx
+++ Client/src/Components/CarouselSwipe.jsx
@@ -1,7 +1,7 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { item1, item2, item3, item4 } from '../assets/index.js';
-import '../Styles/Carousels.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { item1, item2, item3, item4 } from "../assets/index.js";
+import "../Styles/Carousels.css";
 
 export default function CarouselSwipe() {
   const responsive = {
@@ -9,23 +9,23 @@ export default function CarouselSwipe() {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 4,
-      partialVisibilityGutter: 10
+      partialVisibilityGutter: 10,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
-      partialVisibilityGutter: 10
+      partialVisibilityGutter: 10,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      partialVisibilityGutter: 10
+      partialVisibilityGutter: 10,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      partialVisibilityGutter: 10
-    }
+      partialVisibilityGutter: 10,
+    },
   };
 
   return (
