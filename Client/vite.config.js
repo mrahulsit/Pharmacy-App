f82d0241bc17c248019e5d8a83e1e0d1825bd0b1
mrahulsit <mrahuls104@gmail.com>
2024-08-08 14:08:23 +0530

    changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
